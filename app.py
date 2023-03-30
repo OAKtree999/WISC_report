0c312fa46b7a71e6ece454e89cc28cbabbc9b4c7
On An Kook <86966046+OAKtree999@users.noreply.github.com>
2023-03-30 19:03:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -173,7 +173,7 @@ if uploaded_file is not None:
                             # Insert the plotly chart as a picture
                             cell.text = cell.text.replace('<<picture>>', '')
                             run = cell.paragraphs[0].add_run()
-                            run.add_picture(tmp_file.name, width=Inches(6), height=Inches(3))
+                            run.add_picture(tmp_file.name, width=Inches(6), height=Inches(3), font_name='Nanum Gothic')
                     if '<<interpretation>>' in cell.text:
                         # Insert the interpretation text
                         cell.text = cell.text.replace('<<interpretation>>', '')
